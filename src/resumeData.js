4904389630cb7176b0e48cb4345e46a8340cadfa
Sandeep Kapalawai <kapalawai.sandeep.sandeep@gmail.com>
2019-07-28 18:08:39 +0530

    Initial commit to my portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,113 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/sonusandeep/",
+    "name": "Sandeep Kapalawai",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/kapalawai-sandeep-a028b334/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/sonusandeep/",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/sandeepKapal",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"A Computer Science Graduate, an Open Source Evangelist speaks on FOSS(Free and Open Source Softwares), Mozilla's Projects and Products i am  currently working with Mphasis as UI Developer ",
+    "address":"India",
+    "website":"https://SandeepKapalawai.github.io",
+    "education":[
+      {
+        "UniversityName":"Balaji Institute of Engineering and Sciences",
+        "specialization":"Computer Science and Engineering ",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015"
+      },
+      {
+        "UniversityName":"CV Raman Junior College",
+        "specialization":"M.P.C",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2011"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Mphasis",
+        "specialization":"UI Developer",
+        "MonthOfLeaving":"Aug,2018",
+        "YearOfLeaving":"Till date",
+        //"Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Jegan Research Associates",
+        "specialization":"Programmer Analyst",
+        "MonthOfLeaving":"Aug 2015",
+        "YearOfLeaving":"July 2018"
+       
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"JQuery"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Redux"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
